ae5c46b33b93015eaf0ce7be747ad8ef3158b5d4
lingkang <910405656@qq.com>
2019-03-26 14:25:14 +0800

    登陆页

---
+++ app/src/main/java/com/http1it/top/MainActivity.java
@@ -0,0 +1,13 @@
+package com.http1it.top;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
